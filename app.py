2cda9007a458f6ecac85e1c526ee92df07711659
Raga Markely <lmarkely@alum.mit.edu>
2023-06-28 08:52:02 -0400

    chore: Fix column type

--- app.py
+++ app.py
@@ -34,19 +34,19 @@ class Venue(db.Model):
     __tablename__ = "venue"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
     
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    genres = db.Column(db.ARRAY(db.String(120)))
-    website = db.Column(db.String(250))
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(500))
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False)
+    website = db.Column(db.String(250), nullable=False)
+    seeking_talent = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String(500), nullable=False)
     shows = db.relationship("Show", backref="venue", lazy=True)
     
     def __repr__(self):
@@ -56,19 +56,19 @@ class Venue(db.Model):
 class Artist(db.Model):
     __tablename__ = "artist"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    website = db.Column(db.String(250))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(500))
+    website = db.Column(db.String(250), nullable=False)
+    seeking_venue = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String(500), nullable=False)
     shows = db.relationship("Show", backref="artist", lazy=True)
 
     def __repr__(self):
